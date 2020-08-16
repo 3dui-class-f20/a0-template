eb0024db7b0b28dd79723c322c0fc8f93b02ed9e
Blair MacIntyre <blair@macmynatt.com>
2020-08-16 15:00:26 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+            exclude: /node_modules/
+        }]
+    },
+    devServer: {
+        compress: true,
+        disableHostCheck: true
+    }    
+}
