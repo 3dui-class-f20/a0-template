9a49e496df3ab16856e7b8fcea9e3355342f0176
Blair MacIntyre <blair@macmynatt.com>
2020-08-19 09:32:40 -0400

    many small fixes

--- src/index.ts
+++ src/index.ts
@@ -9,27 +9,27 @@ import "@babylonjs/core/Materials/standardMaterial";
 // Required side effects to populate the Create methods on the mesh class. Without this, the bundle would be smaller but the createXXX methods from mesh would not be accessible.
 import {MeshBuilder} from  "@babylonjs/core/Meshes/meshBuilder";
 
-var canvas = document.getElementById("renderCanvas") as HTMLCanvasElement; // Get the canvas element 
-var engine = new Engine(canvas, true); // Generate the BABYLON 3D engine
+const canvas = document.getElementById("renderCanvas") as HTMLCanvasElement; // Get the canvas element 
+const engine = new Engine(canvas, true); // Generate the BABYLON 3D engine
 
 
 /******* Add the Playground Class with a static CreateScene function ******/
 class Playground { 
     public static CreateScene(engine: Engine, canvas: HTMLCanvasElement): Scene {
         // Create the scene space
-        var scene = new Scene(engine);
+        const scene = new Scene(engine);
 
         // Add a camera to the scene and attach it to the canvas
-        var camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, 
+        const camera = new ArcRotateCamera("Camera", Math.PI / 2, Math.PI / 2, 2, 
                                          new Vector3(0,0,5), scene);
         camera.attachControl(canvas, true);
 
         // Add lights to the scene
-        var light1 = new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
-        var light2 = new PointLight("light2", new Vector3(0, 1, -1), scene);
+        new HemisphericLight("light1", new Vector3(1, 1, 0), scene);
+        new PointLight("light2", new Vector3(0, 1, -1), scene);
 
         // Add and manipulate meshes in the scene
-        var sphere = MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
+        MeshBuilder.CreateSphere("sphere", {diameter:2}, scene);
 
         return scene;
     }
@@ -37,12 +37,12 @@ class Playground {
 
 /******* End of the create scene function ******/    
 // code to use the Class above
-var createScene = function() { 
+const createScene = function(): Scene { 
     return Playground.CreateScene(engine, 
         engine.getRenderingCanvas() as HTMLCanvasElement); 
 }
 
-var scene = createScene(); //Call the createScene function
+const scene = createScene(); //Call the createScene function
 
 // Register a render loop to repeatedly render the scene
 engine.runRenderLoop(function () { 
